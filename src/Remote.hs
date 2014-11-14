c6006b037f4e54c0ef21fe7eb1d0cd4c9be1c878
Oliver Braun <ob@obraun.net>
2014-11-14 10:46:50 +0100

    Refactoring

--- src/Remote.hs
+++ src/Remote.hs
@@ -1,8 +1,9 @@
 -- | Helper functions for accessing the remote site.
-module Remote where
+module Remote ( getRemoteRepoList ) where
 
-import           Data.List      (isInfixOf)
-import           System.Process (readProcess)
+import           Control.Applicative ((<$>))
+import           Data.List           (isInfixOf)
+import           System.Process      (readProcess)
 
 -- | Get a list of all repos you have access to (determined by your
 --   SSH key).
@@ -11,25 +12,31 @@ getRemoteRepoList :: String -- ^ The git user on the remote site
                   -> String -- ^ The port
                   -> String -- ^ Get only repos with this string as infix
                   -> IO [String] -- ^ The list of reponames
-getRemoteRepoList user host port infixStr = do
-    -- Output is something like:
-    --
-    -- hello obraun, this is git@gitolite ..
-    --
-    --  R W	braun/14WS/algdatI/testing
-    --  R W	braun/14WS/compiler/testing
-    --  R W	braun/14WS/sweng/testing
-    --  R W	testing
-    --
-    -- More Infos ...
-    rawRepoInfo <- readProcess "ssh" [ "-p" ++ port
-                                     , user ++ "@" ++ host
-                                     , "info"
-                                     ] ""
-    return $ filter (isInfixOf infixStr)
-           $ filter (not . isInfixOf "..*") -- those are not concrete repos
-           $ map (last . words)             -- the last word is the repo name
-           $ takeWhile (not . null)         -- take all 'til the next emptyline
-           $ dropWhile null                 -- drop empty lines
-           $ dropWhile (not . null)         -- drop the header
-           $ lines rawRepoInfo
+getRemoteRepoList user host port infixStr =
+    getRemoteRepoList' infixStr
+        <$> readProcess "ssh" [ "-p" ++ port
+                              , user ++ "@" ++ host
+                              , "info"
+                              ] ""
+
+-- Output is something like:
+--
+-- hello obraun, this is git@gitolite ..
+--
+--  R W	braun/14WS/algdatI/testing
+--  R W	braun/14WS/compiler/testing
+--  R W	braun/14WS/sweng/testing
+--  R W	testing
+--
+-- More Infos ...
+getRemoteRepoList' :: String   -- ^ Get only repos with this string as infix
+                   -> String   -- ^ The raw output from the gitolite info cmd
+                   -> [String] -- ^ The list of reponames
+getRemoteRepoList' infixStr =
+           filter (isInfixOf infixStr)
+           . filter (not . isInfixOf "..*") -- those are not concrete repos
+           . map (last . words)             -- the last word is the repo name
+           . takeWhile (not . null)         -- take all 'til the next emptyline
+           . dropWhile null                 -- drop empty lines
+           . dropWhile (not . null)         -- drop the header
+           . lines
